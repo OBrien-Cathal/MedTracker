18d4d379d55822d83e2c65df54baf1c0d329f684
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-02 16:29:22 +0100

    added PatientsControllerApi

---
+++ src/main/java/com/cathalob/medtracker/controller/api/PatientsControllerApi.java
@@ -0,0 +1,24 @@
+package com.cathalob.medtracker.controller.api;
+
+import com.cathalob.medtracker.model.UserModel;
+import com.cathalob.medtracker.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/patients")
+@RequiredArgsConstructor
+public class PatientsControllerApi {
+
+private final UserService userService;
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getPatientUserModels(Authentication authentication) {
+        return ResponseEntity.ok(userService.getPatientUserModels());
+    }
+}
