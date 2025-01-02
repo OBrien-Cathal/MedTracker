e160dc250261865a97ccddddca2bc67858825340
OBrien-Cathal <obrien.cathal@proton.me>
2025-01-02 18:47:09 +0100

    repository test for getting all PatientRegistrations for a specific practitioner

--- src/test/java/com/cathalob/medtracker/repository/PatientRegistrationRepositoryTests.java
+++ src/test/java/com/cathalob/medtracker/repository/PatientRegistrationRepositoryTests.java
@@ -11,6 +11,10 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import static com.cathalob.medtracker.testdata.RoleChangeBuilder.aRoleChange;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,4 +49,43 @@ class PatientRegistrationRepositoryTests {
         // then - verify the output
         assertThat(saved.getId()).isGreaterThan(0L);
     }
+
+    @Test
+    public void givenSavedPatientRegistrationsForMultiplePractitioners_whenFindByPractitioner_thenReturnPatientRegistrationForOnlyOnePractitioner() {
+        //given - precondition or setup
+
+        UserModel practitioner = UserModelBuilder.aUserModel().withRole(USERROLE.PRACTITIONER).build();
+        UserModel practitioner2 = UserModelBuilder.aUserModel().withRole(USERROLE.PRACTITIONER).build();
+
+        testEntityManager.persist(practitioner);
+        testEntityManager.persist(practitioner2);
+        Stream.iterate(0, n-> n + 1).limit(3).forEach(n -> createAndPersistPatientRegistrationAndRoleChangeForPractitioner(practitioner));
+        createAndPersistPatientRegistrationAndRoleChangeForPractitioner(practitioner2);
+
+        // when - action or the behaviour that we are going test
+        List<PatientRegistration> byPractitionerUserModel = patientRegistrationRepository.findByPractitionerUserModel(practitioner);
+        // then - verify the output
+        assertThat(byPractitionerUserModel).size().isEqualTo(3);
+        assertThat(byPractitionerUserModel).allMatch((e) ->
+                e.getPractitionerUserModel().getId().equals(practitioner.getId())
+                && e.getPractitionerUserModel().getRole().equals(USERROLE.PRACTITIONER));
+    }
+
+    private PatientRegistration createAndPersistPatientRegistrationAndRoleChangeForPractitioner(UserModel practitioner ) {
+        PatientRegistration patientRegistration = new PatientRegistration();
+        RoleChange roleChange = aRoleChange().withNewRole(USERROLE.PATIENT).build();
+
+        patientRegistration.setPractitionerUserModel(practitioner);
+        UserModel registeringUser = roleChange.getUserModel();
+        patientRegistration.setUserModel(registeringUser);
+
+        testEntityManager.persist(registeringUser);
+        testEntityManager.persist(roleChange);
+        patientRegistration.setRoleChange(roleChange);
+
+        testEntityManager.persist(patientRegistration);
+
+        return patientRegistration;
+    }
+
 }
